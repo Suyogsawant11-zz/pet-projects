5b6d6d9739bec28914eec10a053fefb1e79d9582
Suyog <suyog.sawant@coverfoxmail.com>
2019-05-17 12:02:20 +0530

    error handling - resolved, even bit of styling added

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -9,18 +9,21 @@ class ErrorBoundary extends React.Component<any,IErrorState> {
 
     componentDidCatch(error, info) {
         // Display fallback UI
-        this.setState({ hasError: true, error })
+        this.setState({ hasError: true, error:error.message })
     }
 
     render() {
+
         let {hasError, error} = this.state
 
         if (hasError) {
             // You can render any custom fallback UI
             return (
-                <div>
-                    <h1>Something went wrong.</h1>
-                    <h2>{error}</h2>
+                <div className="alert alert-danger" role="alert">
+                    <h4 className="alert-heading">Something went wrong!</h4>
+                    <p>{error}</p>
+                    <hr />
+                    <p className="mb-0">Kindly refresh the page and try again</p>
                 </div>
             )
         }
